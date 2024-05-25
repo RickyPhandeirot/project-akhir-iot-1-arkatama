28d05919123b61c36313309c583b26ab46a8ab32
Ricky Phandeirot <rickyphandeirot@gmail.com>
2024-05-25 22:47:33 +0800

    membuat usercontroller, membuat halaman user, membuat breadcrumbs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,34 +9,35 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('pages.dashboard2');
+    $data['title'] = 'Dashboard';
+        $data['breadcrumbs'][]= [
+            'title' => 'Dashboard',
+            'url' => route('dashboard')
+        ];
+    return view('pages.dashboard2', $data);
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/coba', function () {
-    return view('pages.coba');
-})->middleware(['auth', 'verified']);
 
 Route::get('/led-control', function () {
     return view('pages.ledcontrol');
-})->middleware(['auth', 'verified']);
+})->middleware(['auth', 'verified'])->name('led-control');
 
 Route::get('/sensor', function () {
     return view('pages.sensor');
-})->middleware(['auth', 'verified']);
+})->middleware(['auth', 'verified'])->name('sensor');
 
 Route::get('/user', function () {
     return view('pages.pengguna');
 })->middleware(['auth', 'verified']);
 
-Route::get('/arkatama', function () {
-    return view('pages.arkatama');
-})->middleware(['auth', 'verified']);
-
-
+// route yang hanya boleh diakses jika sudah login
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Users
+    Route::get('users', [UserController::class, 'index'])->name('users.index');
 });
+
 
 require __DIR__.'/auth.php';
