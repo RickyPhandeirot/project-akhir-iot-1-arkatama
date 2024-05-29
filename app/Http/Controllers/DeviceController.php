25adc15713cc5825664f6b53d3101c0f726ef27a
Ricky Phandeirot <rickyphandeirot@gmail.com>
2024-05-29 14:24:40 +0800

    Membuat DeviceController terpisah agar membedakan tangkapan API

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -29,60 +29,60 @@ class DeviceController extends Controller
     }
 
 
-    public function store(Request $request)
-    {
-        $device = new Device;
-        $device->device_name = $request->device_name;
-        $device->current_value = $request->current_value;
-        $device->save();
-        return response()->json([
-            "message" => "Device telah Ditambahkan."
-        ], 201);
-    }
+    // public function store(Request $request)
+    // {
+    //     $device = new Device;
+    //     $device->device_name = $request->device_name;
+    //     $device->current_value = $request->current_value;
+    //     $device->save();
+    //     return response()->json([
+    //         "message" => "Device telah Ditambahkan."
+    //     ], 201);
+    // }
 
-    public function show(string $id)
-    {
-        return Device::find($id);
-    }
+    // public function show(string $id)
+    // {
+    //     return Device::find($id);
+    // }
 
-    public function update(Request $request, string $id)
-    {
-        if (Device::where('id', $id)->exists()) {
-            $device = Device::find($id);
-            $device->device_name = is_null($request->device_name) ? $device->device_name : $request->device_name;
-            $device->current_value = is_null($request->current_value) ? $device->current_value : $request->current_value;
-            $device->save();
-            return response()->json([
-                "message" => "Device telah diupdate."
-            ], 201);
-           } else {
-            return response()->json([
-                "message" => "Device tidak ditemukan."
-           ], 404);
-        }
-    }
+    // public function update(Request $request, string $id)
+    // {
+    //     if (Device::where('id', $id)->exists()) {
+    //         $device = Device::find($id);
+    //         $device->device_name = is_null($request->device_name) ? $device->device_name : $request->device_name;
+    //         $device->current_value = is_null($request->current_value) ? $device->current_value : $request->current_value;
+    //         $device->save();
+    //         return response()->json([
+    //             "message" => "Device telah diupdate."
+    //         ], 201);
+    //        } else {
+    //         return response()->json([
+    //             "message" => "Device tidak ditemukan."
+    //        ], 404);
+    //     }
+    // }
 
-    public function destroy(string $id)
-    {
-        if (Device::where('id', $id)->exists()) {
-            $device = Device::find($id);
-            $device->delete();
-            return response()->json([
-                "message" => "Device telah dihapus."
-            ], 201);
-            } else {
-            return response()->json([
-                "message" => "Device tidak ditemukan."
-            ], 404);
-            }
-    }
+    // public function destroy(string $id)
+    // {
+    //     if (Device::where('id', $id)->exists()) {
+    //         $device = Device::find($id);
+    //         $device->delete();
+    //         return response()->json([
+    //             "message" => "Device telah dihapus."
+    //         ], 201);
+    //         } else {
+    //         return response()->json([
+    //             "message" => "Device tidak ditemukan."
+    //         ], 404);
+    //         }
+    // }
 
-    public function web_index(){
-        return view('pages.sensor', [
-            "title"=> "devices",
-            "devices" => Device::all()
-        ]);
-    }
+    // public function web_index(){
+    //     return view('pages.sensor', [
+    //         "title"=> "devices",
+    //         "devices" => Device::all()
+    //     ]);
+    // }
 
     // public function web_show($id){
     //     return view('layouts.datasensor', [
--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DeviceController;
+// use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\DataController;
 use App\Http\Controllers\LedController;
 use App\Http\Controllers\LedStatusController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\DeviceController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
