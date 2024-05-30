33c9db061e85a7fec6937e71ee68f3ae920ff3b9
Ricky Phandeirot <rickyphandeirot@gmail.com>
2024-05-31 01:03:43 +0800

    Membuat ledcontrol.blade bisa mengatur led rgb dari web

--- resources/views/pages/ledcontrol.blade.php
+++ resources/views/pages/ledcontrol.blade.php
@@ -3,15 +3,20 @@
 @section('content')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
-        /* CSS untuk ikon LED */
-        .led-on {
-            color: yellow;
-            /* Ganti warna sesuai keinginan Anda */
+        .led-on-red {
+            color: rgb(255, 0, 0);
         }
 
-        .led-off {
-            color: rgb(206, 30, 30);
-            /* Ganti warna sesuai keinginan Anda */
+        .led-off-red {
+            color: rgb(100, 0, 0);
+        }
+
+        .led-on-blue {
+            color: rgb(0, 0, 255);
+        }
+
+        .led-off-blue {
+            color: rgb(0, 0, 100);
         }
     </style>
 
@@ -25,10 +30,9 @@
                             <div class="card-body">
                                 <div class="d-flex w-100 justify-content-between">
                                     <div class="d-flex align-items-start text-primary">
-                                        <i class="fas fa-lightbulb fa-fw fa-4x led-icon led-1-icon led-off"></i>
+                                        <i class="fas fa-lightbulb fa-fw fa-4x led-icon led-1-icon led-off-red"></i>
                                         <div>
                                             <h6 class="p-0 m-0 fw-bold">LED 1</h6>
-                                            <p class="p-0 m-0 text-muted">Pin: 18, 4, 5</p>
                                             <div>
                                                 <div class="custom-control custom-switch">
                                                     <input type="checkbox" class="custom-control-input" id="customSwitch1"
@@ -50,10 +54,9 @@
                             <div class="card-body">
                                 <div class="d-flex w-100 justify-content-between">
                                     <div class="d-flex align-items-start text-primary">
-                                        <i class="fas fa-lightbulb fa-fw fa-4x led-icon led-2-icon led-off"></i>
+                                        <i class="fas fa-lightbulb fa-fw fa-4x led-icon led-2-icon led-off-blue"></i>
                                         <div>
                                             <h6 class="p-0 m-0 fw-bold">LED 2</h6>
-                                            <p class="p-0 m-0 text-muted">Pin: 18, 4, 5</p>
                                             <div>
                                                 <div class="custom-control custom-switch">
                                                     <input type="checkbox" class="custom-control-input" id="customSwitch2"
@@ -76,44 +79,56 @@
     <!-- Include the MQTT.js library -->
     <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
     <script>
-      document.addEventListener('DOMContentLoaded', (event) => {
-        const brokerUrl = 'wss://n69919ff.ala.asia-southeast1.emqxsl.com:8084/mqtt';  // Ubah port sesuai dengan pengaturan broker
-        const topicPrefix = 'esp32/led';
+        document.addEventListener('DOMContentLoaded', (event) => {
+            const brokerUrl =
+                'wss://n69919ff.ala.asia-southeast1.emqxsl.com:8084/mqtt'; // Ubah port sesuai dengan pengaturan broker
+            const topicPrefix = 'esp32/led';
 
-        const options = {
-          username: 'Mentoring',
-          password: 'munyenyo',
-          protocol: 'wss',
-          rejectUnauthorized: false // Ini penting jika menggunakan sertifikat yang dihasilkan sendiri (self-signed)
-        };
+            const options = {
+                username: 'Mentoring',
+                password: 'munyenyo',
+                protocol: 'wss',
+                rejectUnauthorized: false // Ini penting jika menggunakan sertifikat yang dihasilkan sendiri (self-signed)
+            };
 
-        const client = mqtt.connect(brokerUrl, options);
+            const client = mqtt.connect(brokerUrl, options);
 
-        client.on('connect', function() {
-          console.log('Connected to MQTT broker');
-        });
+            client.on('connect', function() {
+                console.log('Connected to MQTT broker');
+            });
 
-        client.on('error', function(err) {
-          console.error('Connection error: ', err);
-        });
+            client.on('error', function(err) {
+                console.error('Connection error: ', err);
+            });
 
-        window.toggleLed = function(ledId) {
-          var icon = document.querySelector('.led-' + ledId + '-icon');
-          var ledSwitch = document.getElementById('customSwitch' + ledId);
-          if (icon && ledSwitch) {
-            if (ledSwitch.checked) {
-              icon.classList.remove('led-off');
-              icon.classList.add('led-on');
-            } else {
-              icon.classList.remove('led-on');
-              icon.classList.add('led-off');
+            window.toggleLed = function(ledId) {
+                var icon = document.querySelector('.led-' + ledId + '-icon');
+                var ledSwitch = document.getElementById('customSwitch' + ledId);
+                if (icon && ledSwitch) {
+                    if (ledSwitch.checked) {
+                        icon.classList.remove('led-off-red', 'led-off-blue');
+                        if (ledId === 1) {
+                            icon.classList.add('led-on-red');
+                        } else if (ledId === 2) {
+                            icon.classList.add('led-on-blue');
+                        }
+                    } else {
+                        icon.classList.remove('led-on-red', 'led-on-blue');
+                        if (ledId === 1) {
+                            icon.classList.add('led-off-red');
+                        } else if (ledId === 2) {
+                            icon.classList.add('led-off-blue');
+                        }
+                    }
+
+                    const state = ledSwitch.checked ? 'on' : 'off';
+                    client.publish(`${topicPrefix}/${ledId}`, JSON.stringify({
+                        led: ledId,
+                        state: state
+                    }));
+                    console.log(`LED ${ledId} is ${state}`);
+                }
             }
-
-            const state = ledSwitch.checked ? 'on' : 'off';
-            client.publish(`${topicPrefix}/${ledId}`, JSON.stringify({ led: ledId, state: state }));
-            console.log(`LED ${ledId} is ${state}`);
-          }
-        }
-      });
+        });
     </script>
 @endsection
