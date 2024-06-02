cffbb29eb74b746be2e9afe81cee632a6c7214ac
Ricky Phandeirot <rickyphandeirot@gmail.com>
2024-06-02 21:00:43 +0800

    mengedit console.php untuk cronjob

--- routes/console.php
+++ routes/console.php
@@ -3,9 +3,10 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use Illuminate\Console\Command;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-// schedule::command('check:gas-levels')->everyFiveMinutes();
+// Schedule::command('check:gas-levels')->everyMinute()->sendOutputTo("laravel.log");
